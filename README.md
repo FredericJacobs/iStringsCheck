34ce42facb7c29734f05a12e8a5c394775523cec
Frederic Jacobs <github@fredericjacobs.com>
2015-05-20 21:26:48 +0200

    v1

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# localizedFormatStringsCheck.rs
-Detects malformed a format specified string localizations for Objective-C .strings files.
+# iStringsCheck
+
+iStringsCheck is an utility to verify completeness of .strings (iOS & OS X) translations files and the count of their format attributes.
+
+## Motivation
+
+It is common to crowdsource translations using platforms like Transifex. Unfortunately, Transifex [does not verify that translations do contain the same number of occurences of a given formatter](https://twitter.com/transifex/status/601092349381357568). This is particularly an issue with Objective-C and C-related languages that do not provide any kind of memory safety guarantees on formatted strings.
+
+## Usage
+```
+istringscheck <source_strings> <translations_dir>
+```
+## Verifications
+
+Currently iStringsCheck only verifies whether all localization files contain all of the localization keys and that for a given key, the number of occurences of the formatter %@ is the same as a source language.
+
+## About format strings attacks
+If you are taking input from a user or other untrusted source and displaying it, you need to be careful that your display routines do not process format strings received from the untrusted source. For example, in the following code the syslog standard C library function is used to write a received HTTP request to the system log. Because the syslog function processes format strings, it will process any format strings included in the input packet:
+
+```
+/* receiving http packet */
+int size = recv(fd, pktBuf, sizeof(pktBuf), 0);
+if (size) {
+syslog(LOG_INFO, "Received new HTTP request!");
+syslog(LOG_INFO, pktBuf);
+}
+```
+Many format strings can cause problems for applications. For example, suppose an attacker passes the following string in the input packet:
+
+```
+"AAAA%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x.%n"
+```
+
+This string retrieves eight items from the stack. Assuming that the format string itself is stored on the stack, depending on the structure of the stack, this might effectively move the stack pointer back to the beginning of the format string. Then the %n token would cause the print function to take the number of bytes written so far and write that value to the memory address stored in the next parameter, which happens to be the format string. Thus, assuming a 32-bit architecture, the AAAA in the format string itself would be treated as the pointer value 0x41414141, and the value at that address would be overwritten with the number 76.
+
+Doing this will usually cause a crash the next time the system has to access that memory location, but by using a string carefully crafted for a specific device and operating system, the attacker can write arbitrary data to any location. See the manual page for printf for a full description of format string syntax.
+
+Source: [Secure Coding Guide](https://developer.apple.com/library/mac/documentation/Security/Conceptual/SecureCodingGuide/Articles/ValidatingInput.html)
---
+++ src/main.rs
@@ -0,0 +1,144 @@
+extern crate rustc_serialize;
+extern crate docopt;
+extern crate regex;
+
+use docopt::Docopt;
+use regex::Regex;
+
+use std::collections::HashMap;
+use std::ffi::OsString;
+use std::fs;
+use std::io::prelude::*;
+use std::path::Path;
+use std::path::PathBuf;
+use std::process::Command;
+
+// Write the Docopt usage string.
+static USAGE: &'static str = "
+Usage: istringscheck <source_strings> <translations_dir>
+Options:
+    -h, --help  Displays this message.
+";
+
+#[derive(Debug, RustcDecodable)]
+struct Args {
+    arg_source: String,
+    arg_dir: String,
+}
+
+fn main() {
+    let args: Args = Docopt::new(USAGE)
+                            .and_then(|d| d.decode())
+                            .unwrap_or_else(|e| e.exit());
+    validate_args(&args);
+    let source_strings = hashmap_from_source(args.arg_source);
+
+    for language_file in language_files_from_dir(&args.arg_dir).iter() {
+        println!( "Parsing language file {}", language_file.display());
+
+        let translated_strings = hashmap_from_source(language_file.to_str().unwrap().to_string());
+
+        compare_strings(&source_strings, &translated_strings);
+
+    }
+}
+
+fn compare_strings(source_strings :&HashMap<String, usize>, translated_strings: &HashMap<String, usize>) {
+    for (key, value) in source_strings {
+        let translated_value = translated_strings.get(key).unwrap_or_else(|| panic!("Language file is missing strings for key {}", key));
+
+        if translated_value != value {
+            println!("This attributed string: {} doesn't have the correct amount of occurences of the format argument", key);
+        }
+    }
+}
+
+fn language_files_from_dir(dir_string: &String) -> Vec<PathBuf> {
+    let mut paths = fs::read_dir(&Path::new(dir_string)).unwrap();
+    let mut string_files :Vec<PathBuf> = vec![];
+
+    loop {
+        match paths.next() {
+            Some(x) => {
+                let path = x.unwrap().path();
+                let path_string: String = path.to_str().unwrap().to_string();
+
+                if is_folder(path.clone()){
+                    for file in &language_files_from_dir(&path_string) {
+                        string_files.push(file.clone());
+                    }
+                } else {
+                    let extension = path.extension().unwrap_or(&OsString::new()).to_os_string().into_string().unwrap();
+                    if extension == "strings" {
+                        string_files.push(path);
+                    }
+                }
+            },
+            None => { break }
+        }
+    }
+
+    return string_files;
+}
+
+fn hashmap_from_source(source_path: String) -> HashMap<String, usize> {
+    let mut source_strings: HashMap<String, usize> = HashMap::new();
+    // Apple Strings files are UTF-16 encoded, processing them in UTF-8
+    let utf8_conversion_out = Command::new("iconv")
+                                .arg("-f").arg("utf-16")
+                                .arg("-t").arg("utf-8")
+                                .arg(&source_path).output()
+                                .unwrap_or_else(|e| { panic!("failed to execute process: {}", e) });
+
+    let strings_file = String::from_utf8(utf8_conversion_out.stdout).unwrap();
+    let mut lines  = strings_file.lines();
+
+    let string_key_re = match Regex::new(r#"^"(.*)" = "(.*)";$"#) {
+        Ok(re) => re,
+        Err(err) => panic!("{}", err),
+    };
+    let format_strings_re = match Regex::new(r#"%"#) {
+        Ok(re) => re,
+        Err(err) => panic!("{}", err),
+    };
+
+    loop {
+        match lines.next() {
+            Some(line) => {
+                for cap in string_key_re.captures_iter(&line) {
+                    let n_formats = format_strings_re.captures_iter(&cap.at(2).unwrap()).count();
+                    source_strings.insert(cap.at(1).unwrap().to_string(), n_formats);
+                }
+            },
+            None => { break }
+        }
+    }
+
+    return source_strings;
+}
+
+fn path_from_string(path_string: &String) -> PathBuf {
+    return Path::new(path_string).to_path_buf();
+}
+
+fn is_file(path :PathBuf) -> bool {
+    return fs::metadata(path).map(|m| m.is_file()).unwrap_or(false);
+}
+
+fn is_folder(path :PathBuf) -> bool {
+    return fs::metadata(path).map(|m| m.is_dir()).unwrap_or(false);
+}
+
+// Validates the arguments, checking file and folder exists.
+fn validate_args(args: &Args) {
+    let source_path = path_from_string(&args.arg_source);
+    let dir_path    = path_from_string(&args.arg_dir);
+
+    let source_exists = is_file(source_path);
+    let dir_exists    = is_folder(dir_path);
+
+    if !(source_exists && dir_exists) {
+        panic!("The arguments passed must be the source localization files
+        and the folder containing other localizations \n".to_string() + USAGE);
+    }
+}
